dc8122f6715edac02009b6a8570274d257a2a1ca
vagrant <vagrant@precise64.(none)>
2014-04-15 07:15:24 +0000

    added table

--- public/core.js
+++ public/core.js
@@ -44,5 +44,17 @@ scotchTodo.controller('mainController', function($scope, $http) {
 });
 
 
+scotchTodo.controller('DemoCtrl',  function($scope) {
+    
+    $scope.users = [
+            {name: "Moroni", age: 50},
+            {name: "Tiancum", age: 43},
+            {name: "Jacob", age: 27},
+            {name: "Nephi", age: 29},
+            {name: "Enos", age: 34}
+        ];
+
+});
+
 
 
